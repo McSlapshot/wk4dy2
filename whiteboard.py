c4c777be80441f6061bffc057928f252cf6b1dfa
Sia McQuillan <mcqui064@d.umn.edu>
2022-10-12 17:04:23 -0500

    movies

---
+++ whiteboard.py
@@ -0,0 +1,20 @@
+#Lucky Numbers
+#Given an array of integers arr, a lucky integer is an integer which has a frequency in the array
+#equal to its value.
+#Return a lucky integer in the array. If there are multiple lucky integers return the largest of them.
+#If there is no lucky integer return -1.
+#Example 1:
+#Input: arr = [2,2,3,4]					
+#Output: 2
+#Explanation: The only lucky number in the array is 2 because frequency[2] == 2.
+##Example 2:
+#Input: arr = [1,2,2,3,3,3]
+#Output: 3
+#Explanation: 1, 2 and 3 are all lucky numbers, return the largest of them.
+from collections import Counter
+
+def lucky_num(list):
+    lucky = Counter(list)
+    return lucky.most_common(1)[0][0]
+list = [2,2,3,4]
+print(lucky_num(list))
